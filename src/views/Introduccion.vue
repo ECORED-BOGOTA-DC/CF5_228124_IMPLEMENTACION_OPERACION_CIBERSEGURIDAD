dc181de410edc2abadc456b8d7268d3fa06ac0a0
Luis Pérez <ljpm9401@gmail.com>
2023-12-11 15:51:58 -0500

    Ajustes

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,9 +11,6 @@
     .cont_intro1.pt-4
       p(data-aos="fade-right") La ciberseguridad se fundamenta en la seguridad de la información en el ciberespacio, mundo digital o cibernético, que apropia la seguridad informática o digital, abarcando aspectos de la geopolítica con alcances, incluso, sociales. Esta es otra razón más por la que es importante realizar una adecuada planificación, entrenamiento y concienciación en ciberseguridad para una adecuada implementación posterior de las estrategias de seguridad digital.
       p.mt-4(data-aos="fade-left") La planeación es un elemento transversal para una mejor administración y gestión, en cualquier tipo de proceso o proyecto. Luego, ha de abordarse la planeación como un aspecto importante de la aplicación de estrategias de ciberseguridad. En este componente de formación se obtendrá una amplia y detallada explicación de los elementos requeridos para los procesos de Planificación, Entrenamiento y Concienciación en la implementación de estrategias de ciberseguridad.
-      figure.mt-4(data-aos="zoom-in")
-        .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
 
